be5b4ca6ed67618f5c2655ecfafe702052d5cecd
gagardner <glngrdnr@gmail.com>
2022-06-19 14:25:56 +0200

    Add processNum() & processFunc() to script.js

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
-let buttons = document.querySelectorAll(`.num-button`);
+let numButtons = document.querySelectorAll(`.num-button`);
+let funcButtons = document.querySelectorAll(`.func-button`);
 let display = document.querySelector(`.display`);
-let text = ``;
+//let userInput = ``;
+let firstOperand = ``;
+let secondOperand = ``;
+let operator = null;
+//let secondOperator = null;
 
-buttons.forEach((button) =>{
+numButtons.forEach((button) =>{
     button.addEventListener("click", function() {
-    text += button.textContent;
-    display.textContent = text;        
+    //userInput += button.textContent;
+    processNum(button.textContent);
+    //display.textContent = text;        
     });
 });
+
+funcButtons.forEach((button) => {
+    button.addEventListener("click", function() {
+        processFunc(button.textContent);
+    });
+});
+
+function processNum(input) {
+    if (operator === null) {
+        firstOperand += input;
+        display.textContent = firstOperand;
+    } else if (operator !== null) {
+        secondOperand += input;
+        display.textContent = secondOperand;
+    }
+}
+
+function processFunc(input) {
+    if (input === `=`) {
+        let result = operate(operator, +firstOperand, +secondOperand);
+        display.textContent = result;
+    }
+    if (firstOperand) {
+        operator = input;
+    }
+}
 
 function add(a, b) {
     return a + b;
