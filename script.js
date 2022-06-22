1f8856a0e1bd72571620f43f3323e12785898f8a
gagardner <glngrdnr@gmail.com>
2022-06-22 18:58:03 +0200

    Add functionality to delete input

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ let numButtons = document.querySelectorAll(`.num-button`);
 let funcButtons = document.querySelectorAll(`.func-button`);
 let equalsButton = document.querySelector(`.equals-button`);
 let decimalButton = document.querySelector(`.decimal-button`);
+let delButton = document.querySelector(`.del-button`);
 let display = document.querySelector(`.display`);
 
-let firstOperand = ``;
-let secondOperand = ``;
+let firstOperand = `0`;
+let secondOperand = `0`;
 let operator = ``;
 let result = ``;
 let lastOperator = ``;
 let secondOperandStore = ``;
-//let secondOperator = ``;
 
 numButtons.forEach((button) =>{
     button.addEventListener("click", function() {
@@ -36,14 +36,26 @@ decimalButton.addEventListener("click", () => {
     updateDisplay();
 });
 
+delButton.addEventListener("click", () => {
+    deleteInput();
+    updateDisplay();
+})
+
 function processNum(input) {
     if (operator === ``) {
-        if (firstOperand.length === 0 && input === `0`) {
+        if (firstOperand === 0 && input === `0`) {
             return;
+        } else if (firstOperand === `0` && input !== 0) {
+            firstOperand = input;
+        } else {
+            firstOperand += input;
         }
-        firstOperand += input;
     } else if (operator !== ``) {
-        secondOperand += input;
+        if (secondOperand === `0` && input !== 0) {
+            secondOperand = input;
+        } else {
+            secondOperand += input;
+        }
     }
     secondOperandStore = secondOperand;
     //updateDisplay();
@@ -52,15 +64,13 @@ function processNum(input) {
 function processFunc(input) {
     if (input === `C`) {
         clear();
-    //} else if (input === `=`) {
-    //    equals();
-    } else if (firstOperand !== `` && secondOperand !== ``) {
+    } else if (firstOperand !== `0` && secondOperand !== `0`) {
         equals();
         operator = input;
-    } else if (firstOperand !== ``) {
+    } else if (firstOperand !== `0`) {
         operator = input;
     }
-    //updateDisplay();
+    updateDisplay();
     lastOperator = input;
 }
 
@@ -93,45 +103,61 @@ function operate(operator, a, b) {
 }
 
 function equals() {
-    if (firstOperand !== `` && secondOperand !== ``) {
+    if (firstOperand !== `0` && secondOperand !== `0`) {
         result = operate(operator, firstOperand, secondOperand);
-    } else if (firstOperand !== `` && secondOperand === ``) {
+    } else if (firstOperand !== `0` && secondOperand === `0`) {
         result = operate(lastOperator, firstOperand, secondOperandStore);
     }
 
     firstOperand = result;
-    secondOperand = ``;
+    secondOperand = `0`;
     //operator = ``;
     //updateDisplay();
 }
 
 function clear() {
-    firstOperand = ``;
-    secondOperand = ``;
+    firstOperand = `0`;
+    secondOperand = `0`;
     operator = ``;
     result = ``;
 }
 
 function addDecimal() {
-    if (!firstOperand.includes(`.`)) {
+    if (!firstOperand.includes(`.`) && operator === ``) {
         firstOperand += `.`;
         updateDisplay();
         decimalButton.removeEventListener("click", null);
-    } else if (firstOperand !== `` && operator !== `` && !secondOperand.includes(`.`)) {
+    } else if (operator !== `` && !secondOperand.includes(`.`)) {
         secondOperand += `.`;
         updateDisplay();
         decimalButton.removeEventListener("click", null);
     }
 }
 
-function updateDisplay() {
-    if (result !== `` && secondOperand === ``) {
-        display.textContent = result;
-    } else if (secondOperand !== ``) {
-        display.textContent = secondOperand;
-    } else if (firstOperand !== ``) {
-        display.textContent = firstOperand;
-    } else {
-        display.textContent = ``;
+function deleteInput() {
+    if (operator === `` && firstOperand.length === 1) {
+        firstOperand = `0`;
+    } else if (operator === ``) {
+        firstOperand = firstOperand.slice(0, -1);
+    } else if (operator !== `` && secondOperand.length === 1) {
+        secondOperand = `0`;
+    } else if (operator !== ``) {
+        secondOperand = secondOperand.slice(0, -1);
     }
 }
+
+function updateDisplay() {
+    if (result !== ``) {
+        display.textContent = result;
+    } else if (operator !== `` && secondOperand.length > 1) {
+        display.textContent = secondOperand;
+    } else if (operator !== `` && secondOperand.length === 1) {
+        display.textContent = firstOperand;
+    } else if (operator === ``) {
+        display.textContent = firstOperand;
+    } else {
+        display.textContent = `0`;
+    }
+}
+
+updateDisplay();
